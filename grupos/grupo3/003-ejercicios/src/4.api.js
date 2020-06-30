f2ce43da45869d9723648f359bb485b334c5dac8
Lisandro <lisandro.mehring@est.fi.uncoma.edu.ar>
2020-06-30 03:38:57 -0300

    Ejercicio 003- Punto 4 API PromiseChaining

--- grupos/grupo3/003-ejercicios/src/4.api.js
+++ grupos/grupo3/003-ejercicios/src/4.api.js
@@ -1,4 +1,17 @@
 import fetch from 'node-fetch';
 
-// fetch('..... aquí va la url')
-// recordar que fetch devuelve una promise, entonces puede hacer fetch(....).then(resultado => {.... })
+export function chaining() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((response) => {
+      for (let i = 0; i < response.length; i++) {
+        console.log(
+          'Name: ',
+          response[i].name,
+          ' Direction: ',
+          response[i].address
+        );
+      }
+    })
+    .catch((error) => console.error(error));
+}
