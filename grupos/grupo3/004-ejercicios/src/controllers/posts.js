8b7d28a086729047b2ed92f49b220be149f86356
Rodo <rlg1860@protonmail.com>
2020-07-15 21:03:00 -0300

    cambios

--- grupos/grupo3/004-ejercicios/src/controllers/posts.js
+++ grupos/grupo3/004-ejercicios/src/controllers/posts.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fetch from 'node-fetch'; 
 
 var postApi = express.Router();
 
@@ -6,8 +7,48 @@ postApi.get('/', function (req, res) {
   res.send('Hola posts!.');
 });
 
-postApi.get('/:id', function (req, res) {
-  res.send(`Hola post ${req.params.id}.`);
-});
+postApi.get ('/'), async function(req,res) {
+
+  try {
+   let respPosts = await fetch (  'https://jsonplaceholder.typicode.com/posts/' );
+   
+   let respJsonPosts = await respuestaPosts.json();
+    
+   let respuestaUsers = await fetch (  'https://jsonplaceholder.typicode.com/users/' );
+
+   let respJsonUser = await respuestaUsers.json ();
+
+   let respuestaFinal = await respJsonPosts.map ((m) => {  
+     return {
+
+      user: respJsonUser.find ((u) => u.id === m.userID),
+      id: m.id,
+      title: m.title,
+      body: m.body,
+
+     };
+
+     )
+     }   )
+
+
+
+  }
+
+
+
+  catch (error) { return error;}
+};
+
+//postApi.get('/', function (req, res) {
+//  res.send('Hola posts!.');
+//});
+
+
+//postApi.get('/:id', function (req, res) {
+//  res.send(`Hola post ${req.params.id}.`);
+//});
+
+
 
 export default postApi;
--- grupos/grupo3/004-ejercicios/src/index.js
+++ grupos/grupo3/004-ejercicios/src/index.js
@@ -3,15 +3,43 @@ const app = express();
 import postsRoutes from './controllers/posts';
 import albumRoutes from './controllers/albums';
 
+import os from 'os';
+
+import moment from 'moment';
+
 const PORT = 8080;
+let serverStartUpTime=  moment();
 
 app.use('/posts', postsRoutes);
 app.use('/albums', albumRoutes);
 
+moment.locale('es-AR');
+
+const fecha = new Date();
+let fecha2 = moment (fecha);
+const result = {
+  serverCurrentTime: fecha2.format('MMMM Do YYYY, h:mm:ss a'), // En español
+  serverStartUpTime: serverStartUpTime.format('MMMM Do YYYY, h:mm:ss a'), // En español
+  serverUpTime: moment().startOf('hour').fromNow(), // usando moment relative time
+
+  status: {
+    freemem: os.freemem(),
+    totalmem: os.totalmem(),
+    uptime: os.uptime(),
+    hostname: os.hostname(),
+    platform: os.platform(),
+  },
+};
+
+
+
 // Implementar el endpoint de stats aca. GET "/"
 app.get('/', function (req, res) {
-  res.json({ mensaje: 'Hello world!' });
+  res.json({ "mensaje": result });
 });
+
 
 app.listen(PORT);
 console.log(`Express started on port ${PORT}`);
+
+  serverStartUpTime=  moment();
